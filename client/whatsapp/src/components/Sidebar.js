d0c25030e5d0e0a8248c300f1111e39d72e501d5
Daniel Kuang <danielkuang7@github.com>
2021-07-27 08:12:49 -0400

    Added width style to div

--- client/whatsapp/src/components/Sidebar.js
+++ client/whatsapp/src/components/Sidebar.js
@@ -3,9 +3,9 @@ import { Tab, Nav } from 'react-bootstrap'
 
 export default function Sidebar({id}) {
     return (
-        <div>
+        <div style={{ width: '250px'}}>
             <Tab.Container>
-                
+
             </Tab.Container>
 
         </div>
