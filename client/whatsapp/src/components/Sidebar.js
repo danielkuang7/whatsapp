2876c40d282d975106f70c8e94aa617bcdb030aa
Daniel Kuang <danielkuang7@github.com>
2021-07-28 08:15:25 -0400

    Added classname to div

--- client/whatsapp/src/components/Sidebar.js
+++ client/whatsapp/src/components/Sidebar.js
@@ -3,7 +3,7 @@ import { Tab, Nav } from 'react-bootstrap'
 
 export default function Sidebar({id}) {
     return (
-        <div style={{ width: '250px'}}>
+        <div style={{ width: '250px'}} className="d-flex flex-column">
             <Tab.Container>
 
             </Tab.Container>
