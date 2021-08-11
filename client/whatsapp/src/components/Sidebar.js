d16ba323c065b9eb86ad70159da373ab0c28d15c
Daniel Kuang <danielkuang7@github.com>
2021-08-11 08:22:41 -0400

    Set nav variant to tabs

--- client/whatsapp/src/components/Sidebar.js
+++ client/whatsapp/src/components/Sidebar.js
@@ -9,7 +9,7 @@ export default function Sidebar({id}) {
     return (
         <div style={{ width: '250px'}} className="d-flex flex-column">
             <Tab.Container>
-            <Nav>
+            <Nav variant="tabs" className="justify-content-center">
                 <Nav.Item>
                     <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
                 </Nav.Item>
