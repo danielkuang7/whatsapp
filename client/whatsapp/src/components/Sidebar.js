9940bb26a60d2cba6052b885b47fc1e381e958b6
danielkuang7 <danielkuang7@gmail.com>
2021-09-07 08:14:23 -0400

    Added const conversationsOpen

--- client/whatsapp/src/components/Sidebar.js
+++ client/whatsapp/src/components/Sidebar.js
@@ -10,6 +10,8 @@ const CONTACTS_KEY = "contacts"
 
 export default function Sidebar({id}) {
     const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY)
+    const conversationsOpen = activeKey === CONVERSATIONS_KEY
+
     return (
         <div style={{ width: '250px'}} className="d-flex flex-column">
             <Tab.Container activeKey={activeKey} onSelect={setActiveKey}>
@@ -26,7 +28,7 @@ export default function Sidebar({id}) {
                     Your Id: <span className="text-muted">{id}</span>
                 </div>
                 <Button>
-                    
+                    New {}
                 </Button>
                 <Nav.Item>
                     <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
