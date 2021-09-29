f03841f3465628afac8f3af10bf23a0f24143bf5
danielkuang7 <danielkuang7@gmail.com>
2021-09-29 08:16:08 -0400

    Added form to newcontactmodal

--- client/whatsapp/src/components/NewContactModal.js
+++ client/whatsapp/src/components/NewContactModal.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Modal } from 'react-bootstrap'
+import { Modal, Form, Button } from 'react-bootstrap'
 
 export default function NewContactModal({ closeModal}) {
     return (
         <>
         <Modal.Header closeButton>Create Contact</Modal.Header>
         <Modal.Body>
-            
+        <Form></Form>
         </Modal.Body>
         </>
     )
