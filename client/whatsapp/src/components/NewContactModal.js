57126b7306369230be6b6bb3e740961306e3db3f
danielkuang7 <danielkuang7@gmail.com>
2021-09-30 08:09:26 -0400

    Added function handleSubmit to newContactModal

--- client/whatsapp/src/components/NewContactModal.js
+++ client/whatsapp/src/components/NewContactModal.js
@@ -2,11 +2,14 @@ import React from 'react'
 import { Modal, Form, Button } from 'react-bootstrap'
 
 export default function NewContactModal({ closeModal}) {
+    function handleSubmit(){
+        
+    }
     return (
         <>
         <Modal.Header closeButton>Create Contact</Modal.Header>
         <Modal.Body>
-        <Form></Form>
+        <Form onSubmit={handleSubmit}></Form>
         </Modal.Body>
         </>
     )
