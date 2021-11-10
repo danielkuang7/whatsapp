f5ed8a9c548a052665b7f9c5452c971f30af3051
danielkuang7 <danielkuang7@gmail.com>
2021-11-10 08:15:07 -0500

    Variable created for dashboard

--- client/whatsapp/src/components/App.js
+++ client/whatsapp/src/components/App.js
@@ -2,9 +2,17 @@ import React from 'react';
 import Login from './Login'
 import useLocalStorage from '../hooks/useLocalStorage';
 import Dashboard from './Dashboard'
+import { ContactsProvider } from '../contexts/ContactsProvider';
 
 function App() {
   const [id, setId] = useLocalStorage('id')
+
+  const dashboard = (
+    <ContactsProvider>
+      
+    </ContactsProvider>
+  )
+
   return ( 
       id ? <Dashboard id={id} /> : <Login onIdSubmit={setId}/> 
   )
