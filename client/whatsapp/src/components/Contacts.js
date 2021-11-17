7e8e640ef45b871decac161c7bc6e8d3e4edf6e3
danielkuang7 <danielkuang7@gmail.com>
2021-11-17 08:14:40 -0500

    Set contacts equal to useContacts

--- client/whatsapp/src/components/Contacts.js
+++ client/whatsapp/src/components/Contacts.js
@@ -1,6 +1,8 @@
 import React from 'react'
+import { useContacts } from '../contexts/ContactsProvider';
 
 export default function Contacts() {
+    const { contacts } = useContacts()
     return (
         <div>
             Contacts
