787d76c815eba77f3511a28224fd2b9b1304d1c0
danielkuang7 <danielkuang7@gmail.com>
2021-12-22 08:06:16 -0500

    Created check box

--- client/whatsapp/src/components/NewConversationModal.js
+++ client/whatsapp/src/components/NewConversationModal.js
@@ -9,7 +9,11 @@ export default function NewConversationModal() {
         <Modal.Body>
         <Form onSubmit={handleSubmit}>
             {contacts.map(contact => {
-                <Form.Group controlId={contact.id} key={contact.id}></Form.Group>
+                <Form.Group controlId={contact.id} key={contact.id}>
+                    <Form.Check
+
+                    />
+                </Form.Group>
             })}
             <Button type="submit">Create</Button>
         </Form>
