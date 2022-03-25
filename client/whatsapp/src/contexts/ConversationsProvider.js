428f51dd00993b610ae9257ff87ab949de667033
danielkuang7 <danielkuang7@gmail.com>
2022-03-25 08:28:38 -0400

    Returned contact id equal to recipient

--- client/whatsapp/src/contexts/ConversationsProvider.js
+++ client/whatsapp/src/contexts/ConversationsProvider.js
@@ -19,7 +19,7 @@ export function ConversationsProvider({ children }) {
     const formattedConversations = conversations.map(conversations => {
     const recipients = conversation.recipients.map(recipient =>{
         const contact = contacts.find (contact => {
-            
+            return contact.id === recipient
         })
     })
     })
