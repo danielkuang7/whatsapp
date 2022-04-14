13d03da41106f1aba46a93d815f0db2d0169a6a6
danielkuang7 <danielkuang7@gmail.com>
2022-04-14 08:18:51 -0400

    Imported listGroup to conversations.js

--- client/whatsapp/src/components/Conversations.js
+++ client/whatsapp/src/components/Conversations.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { ListGroup } from 'react-bootstrap'
 
 export default function Conversations() {
     return (
-        <div>
-            Conversations
-        </div>
+        <ListGroup variant="flush">
+        {contacts.map(contact => (
+            <ListGroup.Item key={contact.id}>
+               {contact.name} 
+            </ListGroup.Item>
+        ))}
+        </ListGroup>
     )
 }
