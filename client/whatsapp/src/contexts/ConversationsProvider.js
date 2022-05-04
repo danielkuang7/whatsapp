082bc256d5ef4b08b6d8124d5e5524dc6c090d54
danielkuang7 <danielkuang7@gmail.com>
2022-05-04 07:53:34 -0400

    Added selectedConversationIdex as value

--- client/whatsapp/src/contexts/ConversationsProvider.js
+++ client/whatsapp/src/contexts/ConversationsProvider.js
@@ -34,6 +34,7 @@ export function ConversationsProvider({ children }) {
 
     const value = {
         conversations: formattedConversations, 
+        selectedConversationIndex: setSelectedConversationIndex,
         createConversation
     }
 
