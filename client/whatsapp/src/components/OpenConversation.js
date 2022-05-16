e6b4e6d2c97debfbb54d873c6afd242067ecc43c
danielkuang7 <danielkuang7@gmail.com>
2022-05-16 08:11:58 -0400

    Set className to auto flow

--- client/whatsapp/src/components/OpenConversation.js
+++ client/whatsapp/src/components/OpenConversation.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function OpenConversation() {
     return (
         <div className="d-flex flex-column flex-grow-1">
-            <div className="flex-grow-1">
+            <div className="flex-grow-1 overflow-auto">
 
             </div>
         </div>
