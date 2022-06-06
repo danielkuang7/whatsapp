9d2b0da518551ef7b6a1c47c3d3e6e6ff338d585
danielkuang7 <danielkuang7@gmail.com>
2022-06-06 08:17:54 -0400

    Created state for text and setText

--- client/whatsapp/src/components/OpenConversation.js
+++ client/whatsapp/src/components/OpenConversation.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Form, InputGroup } from 'react-bootstrap'
 
 export default function OpenConversation() {
+    const [text, setText] = useState('')
     return (
         <div className="d-flex flex-column flex-grow-1">
             <div className="flex-grow-1 overflow-auto">
